7fa2addf0473762aeaca9e7e8f45aa43e8751d82
Acciente <221thunder@gmail.com>
2018-07-30 00:04:19 +0800

    Change to store result in a list  rather than a queue.

--- wrapper/slave.py
+++ wrapper/slave.py
@@ -3,7 +3,7 @@ import queue
 import subprocess
 
 
-def worker(job_queue, res_queue, disas_lst):
+def worker(job_queue, res_lst, disas_lst):
     """Worker in thread pool."""
     
     while True:
@@ -17,7 +17,7 @@ def worker(job_queue, res_queue, disas_lst):
                                              stdout=subprocess.PIPE)
             distance, _ = bincmp_process.communicate(bytes(bincmp_input, encoding="utf-8"))
             bincmp_process.wait()
-            res_queue.put((bin_name1, bin_name2, distance))
+            res_lst.append((bin_name1, bin_name2, distance))
             
         except queue.Empty:
             break
@@ -28,13 +28,13 @@ def worker(job_queue, res_queue, disas_lst):
 def multithread_bincmp(thread_num, job_queue, disas_lst):
     """Call bincmp with multithread to get pair-wise distance between binaries."""
     
-    res_queue = queue.Queue()
+    res_lst = []
     thread_pool = []
     for i in range(thread_num):
-        thread = threading.Thread(target=worker, args=(job_queue, res_queue, disas_lst))
+        thread = threading.Thread(target=worker, args=(job_queue, res_lst, disas_lst))
         thread.start()
         thread_pool.append(thread)
     for thread in thread_pool:
         thread.join()
         
-    return res_queue
+    return res_lst
